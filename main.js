0b57249efa532bb12d76b72c1eb3f85710cdb691
gluis <gluis@nataloo.com>
2019-10-01 11:43:57 -0400

    updated scss and gitignore

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 window.onload = () => {
 
-  // notifyNonsense.init()
+  notifyNonsense.init()
 
-  const options = {
-    global: true,
-    text: "Dogs like <a href='https://duckduckgo.com'>links</a>!",
-    type: 'nn-alert',
-    event: 'contextmenu'
-  }
-  notifyNonsense.init(options)
+  // const options = {
+  //   global: true,
+  //   text: "Dogs like <a href='https://duckduckgo.com'>links</a>!",
+  //   type: 'nn-alert',
+  //   event: 'contextmenu'
+  // }
+  // notifyNonsense.init(options)
 
 }
